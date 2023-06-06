68c2e6d5f62e49bc1c46e20ea6c39c3b13795ca0
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2023-06-06 20:54:38 +0500

    project in not finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Carousel from '../components/Carousel/Carousel';
 import { Content } from '../components/Content/Content';
 import Features from '../components/Features/Features';
-import Game from '../components/Game';
+// import Game from '../components/Game';
 import Hero from '../components/Hero/Hero';
 import { heroOne, heroTwo, heroThree } from '../data/HeroData';
 
